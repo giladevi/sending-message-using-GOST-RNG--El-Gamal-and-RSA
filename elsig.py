1b168abf355b881278afb19c6890c582d36f96cf
Danest92 <dwiener92@gmail.com>
2020-12-19 16:18:36 +0200

    elsig changed

--- elsig.py
+++ elsig.py
@@ -1,73 +1,48 @@
-#!/usr/bin/env python
-# encoding:UTF-8
-"""
+from Crypto.Util.number import *
+from Crypto import Random
+import Crypto
 
-Implement the ElGamal Digital Signature Scheme.
+import libnum
+import sys
+from random import randint
+import hashlib
 
-Algorithm
-Key Generation
-1. Select a large random prime p and a generator α of Z∗p.
-2. Generate a random integer x such that 1≤x≤p−2. 
-3. Compute y = α**x mod p.
-4. A’s public key is (p, α, y).
-5. A’s private key is x.
+bits=60
+msg="Hello"
 
-Signature Generation
-A generates a signature for a message m (0 ≤ m < p−1) as follows:
-1. Generatea random integer k such that 1≤k≤p−2 and gcd(k,p−1)=1.
-2. Compute r = α**k mod p.
-3. Compute k**−1 mod (p − 1).
-4. Computes=k**−1(m−xr)mod(p−1). 
-5. A’s signature for m is the pair (r, s),
+if len(sys.argv)>1:
+        msg=str(sys.argv[1])
+if len(sys.argv)>2:
+        bits=int(sys.argv[2])
 
-Signature Verification
-A signature (r, s) produced by A can be verified as follows:
-1. Verify that 1 ≤ r ≤ (p−1); if not return False.
-2. Compute v1 = (y**r)(r**s) mod p. 
-3. Compute v2 = α**m mod p. 
-4. Return v1 = v2.
+p = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
+g=2
 
-"""
-import Crypto.Util.number as num
-import random
-import p3_pair
+s= randint(0, p-1)
+v = pow(g,s,p)
 
-def egKey(s):
-	p,a = p3_pair.pair(s)
-	x = random.randint(1, p-2)
-	y = pow(a,x,p)
-	return p, a, x, y
+e= Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
+# e_1=(gmpy2.invert(e, p-1))
+e_1=(libnum.invmod(e, p-1))
+#D=  bytes_to_long(msg.encode('utf-8')) // if you want direct signing
 
-""" Signature Generation 
-"""
-def egGen(p, a, x, m):
-	while 1:
-		k = random.randint(1,p-2)
-		if num.GCD(k,p-1)==1: break
-	r = pow(a,k,p)
-	l = num.inverse(k, p-1)
-	s = l*(m-x*r)%(p-1)
-	return r,s
+D = int.from_bytes(hashlib.sha256(msg.encode()).digest(),byteorder='big' )
 
-""" Signature Verification 
-"""
-def egVer(p, a,	y, r, s, m):
-	if r < 1 or r > p-1 : return False
-	v1 = pow(y,r,p)%p * pow(r,s,p)%p
-	v2 = pow(a,m,p)
-	return v1 == v2
+S_1=pow(g,e, p)
+S_2=((D-s*S_1)*e_1) % (p-1)
 
-#####################################################################
-# Tests
+v_1 = (pow(v,S_1,p)*pow(S_1,S_2,p))%p
+v_2 = pow(g,D,p)
 
-if __name__ == "__main__":
-	
-	message = 36
-	print "Message: ", message
-	prime,alpha,private,public = egKey(10)
-	print "prime,alpha,private,public", prime,alpha,private,public
-	rr,ss = egGen(prime,alpha,private, message)
-	print "rr,ss", rr, ss
-	isValid = egVer(prime, alpha, public, rr, ss, message)
-	print "Valid Signature: " ,  isValid
+print ("Message: %s " % msg)
+print ("g: %s" % g)
+print ("p: %s" % p)
+print ("\nv: %s" % v)
+print ("e: %s" % e)
+print ("\ns: %s" % s)
+
+print ("\nS_1= %s" % S_1)
+print ("S_2=%s" % S_2)
+print ("\nV_1=%s" % v_1)
+print ("v_2=%s" % v_2)
 
